b4e0c377005a2b0800cb998cef442daef5bcecb0
Brian <brianpalumbo13@hotmail.com>
2026-05-08 17:10:07 -0300

    Fix headless mode for GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     storageState: 'storageState.json',
-    headless: false,
+    headless: true,
 
-  launchOptions: {
-    slowMo: 1000,// Slow down actions by 100ms to see them more clearly
-  },
-},  
+  
+  }, 
 
 reporter: 'html',
 });
